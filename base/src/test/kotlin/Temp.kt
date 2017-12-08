a3be8d507219961e3d3f96470914a7d6f221339e
Victor Schappert <vcschapp@gmail.com>
2017-12-08 02:09:15 -0800

    Delete accidentally committed file

--- base/src/test/kotlin/Temp.kt
+++
@@ -1,7 +0,0 @@
-import io.tarro.base.ClassFileVersion
-import io.tarro.base.Versioned
-
-fun foo(bar: Versioned) {
-    val x: ClassFileVersion = bar.firstVersionSupporting
-    println(x.majorVersion)
-}
