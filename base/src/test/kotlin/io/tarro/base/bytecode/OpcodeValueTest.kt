e466e56fecd15bda22232f4aae8ebd2fa27bfe6b
Victor Schappert <vcschapp@gmail.com>
2017-12-28 20:34:13 -0800

    Remove TODO
    This isn't feasible with the way the asm `Opcodes` class lists the
    opcode constants. It just omits too many to make it worth the effort to
    implement.

--- base/src/test/kotlin/io/tarro/base/bytecode/OpcodeValueTest.kt
+++ base/src/test/kotlin/io/tarro/base/bytecode/OpcodeValueTest.kt
@@ -10,7 +10,3 @@ import io.tarro.test.ConstantContainerTest
  * @since 20171220
  */
 class OpcodeValueTest : ConstantContainerTest<OpcodeValue>(OpcodeValue::class)
-
-// TODO: Add asm as a test dependency and reflectively test all the constants
-//       against the ones with the same name in asm (or if there's a JDK
-//       constant container, do same).
