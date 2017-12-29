5aac08ff34ce13360325b5f54f9115984514dd58
Victor Schappert <vcschapp@gmail.com>
2017-12-28 20:29:03 -0800

    Finalize Javadoc for package io.tarro.base.bytecode

--- base/src/main/java/io/tarro/base/attribute/package-info.java
+++ base/src/main/java/io/tarro/base/attribute/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Defines basic data types relating to the structure of attributes within a
- * Java class file.
+ * Defines basic data types and constant values relating to the structure of
+ * attributes within a Java class file.
  *
  * @author Victor Schappert
  * @since 20171016
---
+++ base/src/main/java/io/tarro/base/bytecode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Defines basic data types and constant values relating to the Java Virtual
+ * Machine instruction set.
+ *
+ * @author Victor Schappert
+ * @since 20171126
+ */
+package io.tarro.base.bytecode;
